9f653296aaa6b51becf2dceebdbbfc07f9b4a881
Lee-Scott <leehscott24@gmail.com>
2023-08-09 21:05:48 -0700

    Implemented category search feature dynamicly

--- Spring Backend/spring-boot-fullstack-portfolio/Spring-Portfolio-Backend/src/main/java/com/familyfirstsoftware/Spring/Portfolio/Backend/ecommerce/config/MyDataRestConfig.java
+++ Spring Backend/spring-boot-fullstack-portfolio/Spring-Portfolio-Backend/src/main/java/com/familyfirstsoftware/Spring/Portfolio/Backend/ecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,28 @@ package com.familyfirstsoftware.Spring.Portfolio.Backend.ecommerce.config;
 
 import com.familyfirstsoftware.Spring.Portfolio.Backend.ecommerce.entity.Product;
 import com.familyfirstsoftware.Spring.Portfolio.Backend.ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManger){
+        entityManager = theEntityManger;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -28,5 +42,34 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // call an internal helper method to expose the id
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        // expose entity ids
+
+
+        /*
+        Entity Class:
+        An entity class is a Java class that is annotated with JPA annotations such as @Entity,
+        @Table, @Id, etc. This class represents a table in the database
+         */
+
+        // get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        // get the entity types for the entities
+        for(EntityType tempEntityType : entities){
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // expose the entity ids for the array of entity / domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
